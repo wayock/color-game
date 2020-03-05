0a7d579c3396f5b285ea7fbb5d7a6586b435ce69
Matthew Wayock <wayock@gmail.com>
2020-03-05 16:47:28 -0500

    added new colors and play again button

--- colorGame.js
+++ colorGame.js
@@ -5,6 +5,19 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.getElementById("reset");
+
+resetButton.addEventListener("click", function (){
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++){
+        // add inital colors to squares
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+    resetButton.textContent = "New Colors";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,6 +33,7 @@ for (let i = 0; i < squares.length; i++){
             messageDisplay.textContent = "Correct!!!";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again?";
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Wrong! Pick again.";
