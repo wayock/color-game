d4b981b96928cdea472519a75f4d5e6c6d21daf0
Matthew Wayock <wayock@gmail.com>
2020-03-05 17:33:43 -0500

    added easy/hard buttons

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,47 @@
-let colors = generateRandomColors(6);
+let numSquares = 6;
 
+let colors = generateRandomColors(6);
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
 let resetButton = document.getElementById("reset");
+let easyButton = document.getElementById("easyButton");
+let hardButton = document.getElementById("hardButton");
+
+easyButton.addEventListener("click", function(){
+    easyButton.classList.add("selected");
+    hardButton.classList.remove("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++){
+        if (colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+   
+})
+
+hardButton.addEventListener("click", function(){
+    easyButton.classList.remove("selected");
+    hardButton.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+})
 
 resetButton.addEventListener("click", function (){
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for (let i = 0; i < squares.length; i++){
